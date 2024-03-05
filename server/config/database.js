c49f2b2080c510300d1e10621d8559311e47b264
TheSaviourEking <saviour.eking@outlook.com>
2024-03-05 04:29:34 +0100

    chore: Step 4: Customize the config/database.js file

--- server/config/database.js
+++ server/config/database.js
@@ -9,15 +9,3 @@ module.exports = {
     //logging: false
   }
 }
-
-// module.exports = {
-//   development: {
-//     storage: process.env.DB_FILE,
-//     dialect: "sqlite",
-//     seederStorage: "sequelize",
-//     benchmark: true,
-//     logQueryParameters: true,
-//     typeValidation: true,
-//     // logging: false
-//   },
-// };
